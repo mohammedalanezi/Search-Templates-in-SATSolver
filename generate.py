cee268c71e810c2a260636f8931a46c92be4e7fd
Mohammed Al-Anezi <228255292+mohammedalanezi@users.noreply.github.com>
2025-10-24 17:06:52 -0400

    Update generate.py

--- generate.py
+++ generate.py
@@ -9,7 +9,7 @@ parent_dir = os.path.dirname(script_dir)
 input_path = os.path.join(script_dir, "input.cnf")
 output_path = os.path.join(script_dir, "output.txt")
 
-kissat_path = os.path.join(parent_dir, "kissat-rel-4.0.2", "build", "kissat") # Before testing: Update this to your sat solver's location 
+kissat_path = os.path.join(parent_dir, "cadical-nets-master", "build", "cadical-nets") # Before testing: Update this to your sat solver's location 
 
 start_time = time.time()
 dimacs_elapsed = 0
@@ -17,28 +17,13 @@ kissat_elapsed = 0
 
 clauses = []
 
-if len(sys.argv) < 4:
-	print("Usage: python3 generate.py <order> <symbol_count> <symbol_frequencies> [frequency_squares] [seed]\n") 
-	sys.exit(1)
-
 variableCount = 0
 
-order = int(sys.argv[1])
-symbol_count = int(sys.argv[2])
-frequencies = []
+order = 10
+symbol_count = 2
+frequencies = [6, 4]
 frequency_squares = 4
 seed = 0
-if len(sys.argv) > 2:
-	for i in range(min(symbol_count, len(sys.argv) - 3)):
-		frequencies.append(int(sys.argv[i+3]))
-if len(sys.argv) > 3 + symbol_count:
-	frequency_squares = int(sys.argv[3 + symbol_count])
-if len(sys.argv) > 4 + symbol_count:
-	seed = int(sys.argv[4 + symbol_count])
-
-if len(frequencies) < symbol_count:
-	print(f"Error: Not enough frequencies given; got {len(frequencies)} symbol frequencies but need {symbol_count} (#symbol_frequencies < symbol_count)\n") 
-	sys.exit(1)
 
 def addClause(variables):
 	if len(variables) == 0:
@@ -62,24 +47,78 @@ def addImplicationClause(antecedent, consequent): # conjunction(AND) of all ante
 
 def addOrthogonalityClauses(square1, square2, frequencies1, frequencies2):
 	global variableCount, clauses
-	
+
+	freq_pairs = {} # [position] = (auxiliary, point)
+
 	for s in range(len(frequencies1)):
 		for t in range(len(frequencies2)):
 			expected_orthogonality = frequencies1[s] * frequencies2[t]
 			
-			pair_vars = []
+			point = (s, t)
+			point_pairs = []
+
 			for x in range(order):
 				for y in range(order):
+					if not ((x,y) in freq_pairs):
+						freq_pairs[(x, y)] = {}
 					pair_var = variableCount + 1
 					variableCount += 1
-					pair_vars.append(pair_var)
+					point_pairs.append(pair_var)
+					freq_pairs[(x, y)][point] = pair_var
 					
 					addImplicationClause([pair_var], [get1DIndex(square1, x, y, s)])
 					addImplicationClause([pair_var], [get1DIndex(square2, x, y, t)])
 					
 					addImplicationClause([get1DIndex(square1, x, y, s), get1DIndex(square2, x, y, t)], [pair_var])
+			addCardinalityClauses(point_pairs, expected_orthogonality, expected_orthogonality) # likely unneeded if tryTemplate is true
+				
+	if tryTemplate: # LEMMA 3.2 II and IV, TODO: merge these into fewer for loops
+		for x in range(4):
+			#if (s == 0 and t == 1) or (s == 1 and t == 0):
+			for i in range(2):
+				relation_ii_row = []
+				for y in range(4,10):
+					relation_ii_row.append(freq_pairs[(x, y)][(i, 1 - i)])
+				addCardinalityClauses(relation_ii_row, 3, 3)
+				
+				relation_ii_col = []
+				for y in range(4,10):
+					relation_ii_col.append(freq_pairs[(y, x)][(i, 1 - i)])
+				addCardinalityClauses(relation_ii_col, 3, 3)
+		for x in range(4,10): 
+			#if (s == 1 and t == 1):
+			relation_iv_row = []
+			for y in range(4,10):
+				relation_iv_row.append(freq_pairs[(x, y)][(1, 1)])
+			addCardinalityClauses(relation_iv_row, 2, 2)
 			
-			addCardinalityClauses(pair_vars, expected_orthogonality, expected_orthogonality)
+			relation_iv_col = []
+			for y in range(4,10):
+				relation_iv_col.append(freq_pairs[(y, x)][(1, 1)])
+			addCardinalityClauses(relation_iv_col, 2, 2)
+			
+			#elif (s == 0 and t == 0):
+			relation_iv_row = []
+			for y in range(4,10):
+				relation_iv_row.append(freq_pairs[(x, y)][(0, 0)])
+			addCardinalityClauses(relation_iv_row, 4, 4)
+			
+			relation_iv_col = []
+			for y in range(4,10):
+				relation_iv_col.append(freq_pairs[(y, x)][(0, 0)])
+			addCardinalityClauses(relation_iv_col, 4, 4)
+			
+			#elif (s == 1 and t == 0) or (s == 0 and t == 1):
+			for i in range(2):
+				relation_iv_row = []
+				for y in range(10):
+					relation_iv_row.append(freq_pairs[(x, y)][(i, 1 - i)])
+				addCardinalityClauses(relation_iv_row, 2, 2)
+				
+				relation_iv_col = []
+				for y in range(10):
+					relation_iv_col.append(freq_pairs[(y, x)][(i, 1 - i)])
+				addCardinalityClauses(relation_iv_col, 2, 2)
 
 def addCardinalityClauses(variables, mininum, maximum): # <= maximum variables and >= minimum values are true (latin squares would use minimum = maximum = 1 for each symbol)
 	global variableCount
@@ -133,37 +172,45 @@ def addXORClauses(chain): # create XOR clauses for given chain, should add 2^(le
 				tmpChain[total[i][j]] = -tmpChain[total[i][j]]
 			addClause(tmpChain)
 
-if __name__ == "__main__": # test script, This is a copy of my first frequency square encoding but with sinz cardinality encoding above
+def addLexicographicalOrder(a, b): # lexico ordering
+			# for any 2 vectors in the same sections
+			# a is vector 1 and b is vector 2 
+			# a0 = 1 => b0 = 1, equiv to, b0 = 0 => a0 = 0 (contrapositive)
+			# a0 = b0 => -a1 or b1 				(2 cases a0 and b0 = 0 and a0 and b0 = 1)
+			# a0 = b0 and a1 = b1 => -a2 or b2 	(4 cases, a0 and b0 = 0, a0 and b0 = 1, a1 and b1 = 0, a1 and b1 = 1)
+	addImplicationClause([a[0]], [b[0]])
+	for i in range(2):
+		parityA = i * 2 - 1
+		addImplicationClause([parityA * a[0], parityA * b[0]], [-a[1], b[1]])
+		for j in range(2):
+			parityB = j * 2 - 1
+			addImplicationClause([parityA * a[0], parityA * b[0], parityB * a[1], parityB * b[1]], [-a[2], b[2]])
+
+if __name__ == "__main__": # test this with cadical exhauste, its open sourced on github
 	tryTemplate = True
 
 	def get1DIndex(l, r, c, s):
-		index = 4 * order * symbol_count * r 
-		index += 4 * symbol_count * c 
-		index += symbol_count * l
-		index += s 
-		return index + 1
+		index = 4 * order * r 
+		index += 4 * c 
+		index += l + 1
+		if s == 0:
+			index = -index
+		return index
 
 	def get4DIndex(v): 
 		v = v - 1 
-		r = v // (4 * order * symbol_count)
-		rem = v % (4 * order * symbol_count)
-		c = rem // (4 * symbol_count)
-		rem = rem % (4 * symbol_count)
-		l = rem // symbol_count
-		s = rem % symbol_count
-		return l, r, c, s
+		r = v // (4 * order) 
+		rem = v % (4 * order)
+		c = rem // 4
+		rem = rem % 4
+		l = rem
+		return l, r, c
 
 	variableCount = get1DIndex(frequency_squares - 1, order - 1, order - 1, symbol_count - 1) 
 	for l in range(frequency_squares):
-		for x in range(order):
-			for y in range(order):
-				# Each cell (x,y) has exactly one symbol
-				cell_vars = [get1DIndex(l, x, y, z) for z in range(symbol_count)]
-				addCardinalityClauses(cell_vars, 1, 1)
-		
 		if l >= 2 or not tryTemplate:
 			for x in range(order):
-				for z in range(symbol_count):
+				for z in range(symbol_count): # could reduce to just constraint one of the frequencies
 					# Each symbol z appears "some amount of times" in row x
 					row_vars = [get1DIndex(l, x, y, z) for y in range(order)]
 					addCardinalityClauses(row_vars, frequencies[z], frequencies[z])
@@ -175,6 +222,25 @@ if __name__ == "__main__": # test script, This is a copy of my first frequency s
 					addCardinalityClauses(col_vars, frequencies[z], frequencies[z])
 	
 	if tryTemplate:
+		a = [get1DIndex(2, 5, y+1, 1) for y in range(3)]
+		b = [get1DIndex(2, 6, y+1, 1) for y in range(3)]
+		addLexicographicalOrder(a, b)
+		
+		a=[get1DIndex(2, 7, y+1, 1) for y in range(3)]
+		b=[get1DIndex(2, 8, y+1, 1) for y in range(3)]
+		c=[get1DIndex(2, 9, y+1, 1) for y in range(3)]
+		addLexicographicalOrder(a, b)
+		addLexicographicalOrder(b, c)
+		#addLexicographicalOrder(a, c)
+
+		for i in range(2):
+			a=[get1DIndex(2, 1, y+4 + 3*i, 1) for y in range(3)]
+			b=[get1DIndex(2, 2, y+4 + 3*i, 1) for y in range(3)]
+			c=[get1DIndex(2, 3, y+4 + 3*i, 1) for y in range(3)]
+			addLexicographicalOrder(a, b)
+			addLexicographicalOrder(b, c)
+			#addLexicographicalOrder(a, c)
+
 		for x in range(order):
 			for y in range(order):
 				if x < 4: 
@@ -197,14 +263,24 @@ if __name__ == "__main__": # test script, This is a copy of my first frequency s
 		for i in range(2, frequency_squares):
 			for j in range(i + 1, frequency_squares):
 				addOrthogonalityClauses(i, j, frequencies, frequencies)
-
+				
 		for x in range(order):
 			for y in range(order):
 				bits = []
 				for l in range(frequency_squares):
 					bits.append(get1DIndex(l, x, y, 1))
-				addXORClauses(bits)
-				
+				pass #addXORClauses(bits)
+		
+		for x in range(4,7):
+			addClause([get1DIndex(3, x, 0, 1)])
+			addClause([get1DIndex(3, 0, x, 1)])
+			
+			addClause([get1DIndex(2, x+3, 0, 1)])
+			addClause([get1DIndex(2, 0, x+3, 1)])
+		addClause([get1DIndex(3, 4, 1, 1)])
+		addClause([get1DIndex(2, 4, 2, 1)])
+		addClause([get1DIndex(2, 4, 3, 1)])
+		
 	with open(input_path, "w") as f:
 		f.write(f"p cnf {variableCount} {len(clauses)}\n")
 		f.write("\n".join(clauses))
@@ -217,54 +293,10 @@ if __name__ == "__main__": # test script, This is a copy of my first frequency s
 		commands = [kissat_path, input_path, "--seed=" + str(seed)]
 		subprocess.run(commands, stdout=out_file, stderr=subprocess.STDOUT)
 	kissat_elapsed = round((time.time() - kissat_time) * 100)/100
-	print("Wrote Kissat output to:", output_path)
-	
-	result = [] # the resulting frequency square
-	with open(output_path, "r") as f:
-		satisfiable = None
-		for l in range(frequency_squares):
-			result.append([])
-			for y in range(order):
-				result[l].append([])
-				for x in range(order):
-					result[l][y].append(-1) # easily tells us if logic error occured by the existance of -1 symbol
-		for line in f:
-			if line.startswith("s "):
-				if "UNSATISFIABLE" in line:
-					satisfiable = "UNSAT"
-				elif "SATISFIABLE" in line:
-					satisfiable = "SAT"
-			elif line.startswith("v "):
-				values = line[2:].strip().split()
-				for val in values:
-					if val == '0': # end of variables
-						continue
-					val = int(val)
-					if val > 0 and val <= get1DIndex(frequency_squares - 1, order - 1, order - 1, symbol_count - 1):
-						l, r, c, s = get4DIndex(val)
-						result[l][r][c] = s
+	print("Wrote output to:", output_path)
 
-		print("Result:", satisfiable)
-		if satisfiable == "SAT":
-			print("[",end="")
-			for row in range(order):
-				if row == 0:
-					print("[",end="")
-				else:
-					print(" [",end="")
-				for x in range(order):
-					for l in range(frequency_squares):
-						print(result[l][row][x], end='')
-					if x == order - 1:
-						if row == order - 1:
-							print("]]", end='')
-						else:
-							print("],", end='')
-					else:
-						print(", ", end='')
-				print(" ")
-	print("Total elapsed time of script:", round((time.time() - start_time) * 100)/100, "seconds")
+	print("\nTotal elapsed time of script:", round((time.time() - start_time) * 100)/100, "seconds")
 	print("     Dimacs elapsed time:", dimacs_elapsed, "seconds")
-	print("     Kissat elapsed time:", kissat_elapsed, "seconds")
+	print("     SAT Solver elapsed time:", kissat_elapsed, "seconds")
 
 # cd /mnt/g/Code/sat\ solver\ stuff/search\ templates
